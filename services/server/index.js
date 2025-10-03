d3a68e7d62721a58763a1fc55a163d0d4775bd5e
Patrick Regan <PatrickAndrewRegan@gmail.com>
2025-10-03 15:56:56 -0400

    fix: add missing asyncHandler function and update region
    - Add asyncHandler function to services/server/index.js
    - Update fly.toml to use dfw region instead of deprecated mia
    - Fixes TypeError: asyncHandler is not a function error

--- services/server/index.js
+++ services/server/index.js
@@ -4,7 +4,15 @@ const projectProcessingService = require('./project-processing-service');
 const backfillService = require('./backfill-service');
 const userMigrationService = require('./user-migration-service');
 
+// Async handler utility for Express routes
+function asyncHandler(fn) {
+  return (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
+}
+
 module.exports = {
+  asyncHandler,
   ...commitProcessingService,
   ...colorManagementService,
   ...projectProcessingService,
